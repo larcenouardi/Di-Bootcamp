96a61b44e73c4806ca52a19fb363e5280ab96a37
larcenouardi <contact.ouardilahcen@gmail.com>
2023-05-16 22:51:54 +0100

    "edit numb"

--- WeeK4/Day4/Dailly_challenge_99_Bottles_of-Beer/Script.js
+++ WeeK4/Day4/Dailly_challenge_99_Bottles_of-Beer/Script.js
@@ -1,12 +1,35 @@
-
-let numBottles = parseInt(prompt("How many bottles do you want to start with?"));
+let numBottles = parseInt(prompt("Enter the number of bottles:"));
+let subtracted = 1;
 
 while (numBottles > 0) {
-  let word = numBottles === 1 ? "bottle" : "bottles";
-  console.log(numBottles + " " + word + " of beer on the wall");
-  console.log(numBottles + " " + word + " of beer");
-  console.log("Take " + (numBottles > 1 ? "one" : "it") + " down, pass it around");
-  numBottles--;
-  word = numBottles === 1 ? "bottle" : "bottles";
-  console.log(numBottles === 0 ? "No more bottles of beer on the wall" : numBottles + " " + word + " of beer on the wall");
+  let bottlesText = numBottles === 1 ? "bottle" : "bottles";
+  console.log(`${numBottles} ${bottlesText} of beer on the wall`);
+  console.log(`${numBottles} ${bottlesText} of beer`);
+
+  let passingText = subtracted === 1 ? "it" : "them";
+
+  numBottles -= subtracted;
+  console.log(`Take ${subtracted} down, pass ${passingText} around`);
+  subtracted++;
+
+  let remainingBottlesText = numBottles === 1 ? "bottle" : "bottles";
+  let remainingBottles = numBottles === 0 ? "no" : numBottles;
+
+  if (numBottles === 0) {
+    console.log(`${remainingBottles} bottle of beer on the wall`);
+  }
+  
+  else if (numBottles <= 1) {
+    console.log("No more bottles of beer on the wall");
+    break;
+  }
+
+  
+  
+
+  else {
+    console.log(`${remainingBottles} ${remainingBottlesText} of beer on the wall`);
+  }
+
+  console.log(); 
 }

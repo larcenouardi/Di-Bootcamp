9b7db53ad41f31302ba648e8c510c321d30efdad
larcenouardi <contact.ouardilahcen@gmail.com>
2023-05-17 23:52:36 +0100

    Robot

---
+++ WeeK7/Day5/Mini_Projet_Robo_Friends_Search/Script.js
@@ -0,0 +1,106 @@
+
+const searchBox = document.querySelector('#search-box');
+const robotList = document.querySelector('#robot-list');
+
+let robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      image: 'https://robohash.org/1?200x200'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      image: 'https://robohash.org/2?200x200'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      image: 'https://robohash.org/3?200x200'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      image: 'https://robohash.org/4?200x200'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+      image: 'https://robohash.org/5?200x200'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+      image: 'https://robohash.org/6?200x200'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+      image: 'https://robohash.org/7?200x200'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      image: 'https://robohash.org/8?200x200'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+      image:'https://robohash.org/9?200x200'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+      image:'https://robohash.org/10?200x200'
+    }
+   
+    ];
+
+
+// function to display the list of robots
+function displayRobots(filteredRobots) {
+  const html = filteredRobots.map(robot => `
+    <div class="robot-card">
+      <img src="${robot.image}" alt="${robot.name}">
+      <h2>${robot.name}</h2>
+      <p>${robot.email}</p>
+    </div>
+  `).join('');
+  robotList.innerHTML = html;
+}
+
+
+displayRobots(robots)
+
+// function to filter robots
+function filterRobots() {
+  const searchTerm = searchBox.value.toLowerCase();
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchTerm);
+  });
+  console.log(filteredRobots)
+  displayRobots(filteredRobots);
+}
+
+// Listen to the input keyup to filter robots
+searchBox.addEventListener('keyup', filterRobots);

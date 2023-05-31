d36f8b4c61bd83ce16206719ba1c8bbc97a4e982
larcenouardi <contact.ouardilahcen@gmail.com>
2023-05-31 21:14:59 +0100

    "mod"

--- WeeK9/Day5/Mini_Project_Star_Wars/Script_MP_Star_Wars.js
+++ WeeK9/Day5/Mini_Project_Star_Wars/Script_MP_Star_Wars.js
@@ -12,13 +12,13 @@ function fetchCharacter() {
   // Display loading message
   loadingDiv.style.display = 'block';
 
+  var id = Math.floor(Math.random() * 83)
+
   // Send AJAX request to the API
-  fetch('https://www.swapi.tech/api/people/')
+  fetch(`https://www.swapi.tech/api/people/${id}`)
     .then(response => response.json())
     .then(data => {
-      const characters = data.results;
-      const randomIndex = Math.floor(Math.random() * characters.length);
-      const character = characters[randomIndex];
+      const character = data.result;
 
       displayCharacter(character);
     })
@@ -33,15 +33,26 @@ function fetchCharacter() {
 
 // Function to display the character information
 function displayCharacter(character) {
-  const { name, height, gender, birth_year, homeworld } = character;
+  const { name, height, gender, birth_year, homeworld } = character.properties;
 
   characterInfoDiv.innerHTML = `
     <h2>${name}</h2>
     <p>Height: ${height}</p>
     <p>Gender: ${gender}</p>
     <p>Birth Year: ${birth_year}</p>
-    <p>Homeworld: ${homeworld}</p>
+    <p id="Hmworld">Homeworld</p>
   `;
+
+  fetch(homeworld)
+.then(response => response.json())
+.then(response => {
+
+  document.getElementById("Hmworld").textContent= "Homeworld : "  +response.result.properties.name
+  console.log(response.result.properties.name)
+
+})
+.catch(error => alert("Erreur : " + error));
+
 }
 
 // Function to display error message

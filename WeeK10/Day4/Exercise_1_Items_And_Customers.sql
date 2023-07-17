1cb21e1f2388c9dca27ff14ea1c2279d17709e0a
larcenouardi <contact.ouardilahcen@gmail.com>
2023-07-17 02:14:40 +0100

    sql2

--- WeeK10/Day4/Exercise_1_Items_And_Customers.sql
+++ WeeK10/Day4/Exercise_1_Items_And_Customers.sql
@@ -1,16 +1,16 @@
-All items, ordered by price (lowest to highest):
+-- All items, ordered by price (lowest to highest):
 
 SELECT * FROM items ORDER BY price ASC;
 
-Items with a price above 80 (80 included), ordered by price (highest to lowest):
+-- Items with a price above 80 (80 included), ordered by price (highest to lowest):
 
 SELECT * FROM items WHERE price >= 80 ORDER BY price DESC;
 
-The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results:
+-- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results:
 
 SELECT first_name FROM customers ORDER BY first_name ASC LIMIT 3;
 
-All last names (no other columns!), in reverse alphabetical order (Z-A):
+-- All last names (no other columns!), in reverse alphabetical order (Z-A):
 
 
 SELECT last_name FROM customers ORDER BY last_name DESC;
